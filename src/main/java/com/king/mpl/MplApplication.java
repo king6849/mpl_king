b79c54eb33733ef9542082b277d66664083ea4cd
king <965853365@qq.com>
2019-10-03 20:20:53 +0800

    mpl 1.0version

---
+++ src/main/java/com/king/mpl/MplApplication.java
@@ -0,0 +1,13 @@
+package com.king.mpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MplApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MplApplication.class, args);
+    }
+
+}
